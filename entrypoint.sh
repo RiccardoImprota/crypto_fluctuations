7878e0c939d5650d4fd5328d1d0da4822e507780
RiccardoImprota <72228376+RiccardoImprota@users.noreply.github.com>
2021-09-29 17:34:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,24 +12,34 @@ echo "End script"
 
 while true; do
     currenttime=$(date +%H:%M)
-    if [[ "$currenttime" == "00:01" ]]; then
-        echo "Run"
+    if [[ "$currenttime" == "00:02" ]]; then
+        echo "Updating data"
+        pkill -f servinglayer.py
+        pkill -f apicurrentpercentages.py
+        pkill -f notifier.py
         python3 /app/src/apihistoricprices.py
         python3 /app/src/historicingestion.py
         python3 /app/src/predictor.py
+        python3 /app/src/bandaid.py
+        python3 /app/src/bandaid2.py
+        sleep 10
+        exec python3 src/servinglayer.py &
+        exec python3 src/apicurrentpercentages.py &
+        exec python3 src/notifier.py &
+
     fi
     currentminute=$(date +%M)
-    if [[ "$currentminute" == "30" || "$currentminute" == "00" ]];
+    if [[ "$currentminute" == "15" || "$currentminute" == "30" || "$currentminute" == "45" || "$currentminute" == "00" ]];
     then
         echo "Restart for fault tolerance"
         pkill -f servinglayer.py
         pkill -f apicurrentpercentages.py
         pkill -f notifier.py
-        sleep 1.5
+        sleep 3
         exec python3 src/servinglayer.py &
         exec python3 src/apicurrentpercentages.py &
         exec python3 src/notifier.py &
     fi
-    sleep 58
+    sleep 55
 done
 
